4b9bacb28f5a82def3392462b18a162399dc8684
Steven Degutis <sbdegutis@gmail.com>
2025-06-08 13:24:51 -0500

    Draw solid color via TypeScript.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 extern crate sdl3;
 
 use oxidase::{Allocator, SourceType};
+use rquickjs::prelude::MutFn;
 use sdl3::event::Event;
 use sdl3::keyboard::Keycode;
 use sdl3::pixels::Color;
@@ -10,40 +11,40 @@ use std::time::Duration;
 
 pub fn main() {
     let runtime = rquickjs::Runtime::new().unwrap();
-
     let context = rquickjs::Context::full(&runtime).unwrap();
 
-    let mut sbuf = "const n:number = 3".to_string();
+    let mut sbuf = "drawcolor(128 as number, 0, 0)".to_string();
     let alloc = Allocator::default();
     let _ret = oxidase::transpile(&alloc, SourceType::ts(), &mut sbuf);
     println!("{}", sbuf);
-
-    context.with(|ctx| {
-        ctx.eval::<(), _>(r#" const foo = 3 "#).unwrap();
-    });
 
     let sdl_context = sdl3::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
     let window = video_subsystem
-        .window("rust-sdl3 demo", 800, 600)
+        .window("Mini Game Maker", 800, 600)
         .position_centered()
         .build()
         .unwrap();
 
     let mut canvas = window.into_canvas();
-
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
-
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
+
+    context.with(|ctx| {
+        let f = MutFn::new(move |r: u8, g: u8, b: u8| {
+            canvas.set_draw_color(Color::RGB(r, g, b));
+            canvas.clear();
+            canvas.present();
+        });
+
+        let f = rquickjs::Function::new(ctx.clone(), f)
+            .unwrap()
+            .with_name("hi");
+        let _ = ctx.globals().set("drawcolor", f);
+        ctx.eval::<(), _>(sbuf).unwrap();
+    });
 
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
@@ -57,7 +58,6 @@ pub fn main() {
 
         // The rest of the game loop goes here...
 
-        canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
